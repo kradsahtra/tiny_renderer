3ba55a27d390f86c6c17e22268390162d289cdef
krad <leechannl@icloud.com>
2018-05-22 00:11:33 +0800

    fix lint warning

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-    for (float t = 0.; t < 1.; t += .1) {
-        int x = x0 * (1. - t) + x1 * t;
-        int y = y0 * (1. - t) + y1 * t;
+void line(int x0, int y0, int x1, int y1, TGAImage &image, const TGAColor &color) {
+    for (int t = 0; t < 100; t++) {
+        int x = x0 * (100 - t) / 100 + x1 * t;
+        int y = y0 * (100 - t) / 100 + y1 * t;
         image.set(x, y, color);
     }
 }
