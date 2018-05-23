15cde726fb769c6dc7cd018304a499cad29a6a8b
krad <leechannl@icloud.com>
2018-05-23 12:13:52 +0800

    make the iteration exactly

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
+#include <iostream>
 #include "tga_image.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, const TGAColor &color) {
-    for (int t = 0; t < 100; t++) {
-        int x = x0 * (100 - t) / 100 + x1 * t;
-        int y = y0 * (100 - t) / 100 + y1 * t;
+    int count = x1 - x0;
+    for (int t = 0; t < count; t++) {
+        int x = x0 * (count - t) / count + x1 * t / count;
+        int y = y0 * (count - t) / count + y1 * t / count;
+        std::cout << "x: " << x << ", y: " << y << "\n";
         image.set(x, y, color);
     }
 }
@@ -14,6 +17,8 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, const TGAColor &color
 int main(int argc, char **argv) {
     TGAImage image(100, 100, TGAImage::RGB);
     line(13, 20, 80, 40, image, white);
+    line(20, 13, 40, 80, image, red);
+    line(80, 40, 13, 20, image, red);
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
     return 0;
